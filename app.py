65b01cdecdd60609240fc499726c3cd4c70430b4
hanwool <na990312@naver.com>
2024-01-30 18:40:50 +0900

    6th commit: add tab func

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import plotly.graph_objects as go
 import plotly.express as px 
 
 def load_data():
-    df=pd.read_csv('./data/df.csv') 
+    df=pd.read_csv('./data/df.csv')
     return df
 
 def main():
@@ -17,48 +17,54 @@ def main():
     df=load_data()
     #st.dataframe(df)
     
-    #Frequency of Orders by Days of week
-    target=df.order_dow.value_counts()
-    colors=sns.color_palette('hls',len(target.index)).as_hex()
-    fig=go.Figure()
-    fig.add_trace(
-        go.Bar(x=target.index,y=target,
-                marker={'color':colors,
-                   'line':{'color':'black','width':3},
-                   'pattern':{'shape':'/'}})
-    )
-
-    fig.update_layout(go.Layout(title={'text':'Frequency of Orders by Days of Week',
-                                  'font':{'color':'blue','size':30}},
-                            xaxis={'title':{'text':'Days of Week'},
-                                   'gridwidth':1,'showgrid':True},
-                            yaxis={'title':{'text':'Frequency'},
-                                   'gridwidth':1,'showgrid':True},
-
-    ))
-    st.plotly_chart(fig,use_container_width=True)
     
-    #Frequency of Orders by Hour of Day
-    target=df.order_hour_of_day.value_counts()
-    fig=go.Figure()
-    fig.add_trace(
-        go.Bar(x=target.index,y=target,
-            marker={'color':px.colors.qualitative.Dark24,
-                   'line':{'color':'black','width':3},
-                   'pattern':{'shape':'/'}})
-    )
+    tab1,tab2=st.tabs(['Days of Week','Hour of Day'])
+    with tab1:
+        #Frequency of Orders by Days of week
+        target=df.order_dow.value_counts()
+        colors=sns.color_palette('hls',len(target.index)).as_hex()
+        fig=go.Figure()
+        fig.add_trace(
+            go.Bar(x=target.index,y=target,
+                    marker={'color':colors,
+                    'line':{'color':'black','width':3},
+                    'pattern':{'shape':'/'}})
+        )
 
-    fig.update_layout(go.Layout(title={'text':'Frequency of Orders by Hour of Day',
-                                  'font':{'color':'blue','size':30}},
-                            xaxis={'title':{'text':'Hour_of_Day'},
-                                   'gridwidth':1,'showgrid':True},
-                            yaxis={'title':{'text':'Frequency'},
-                                   'gridwidth':1,'showgrid':True},
-                            margin=dict(l=23,r=23,b=23)
+        fig.update_layout(go.Layout(title={'text':'Frequency of Orders by Days of Week',
+                                    'font':{'color':'blue','size':30}},
+                                xaxis={'title':{'text':'Days of Week'},
+                                    'gridwidth':1,'showgrid':True},
+                                yaxis={'title':{'text':'Frequency'},
+                                    'gridwidth':1,'showgrid':True},
 
-    ))
-    st.plotly_chart(fig,use_container_width=True)
-      
+        ))
+        st.plotly_chart(fig,use_container_width=True)
+    with tab2:
+        #Frequency of Orders by Hour of Day
+        target=df.order_hour_of_day.value_counts()
+        fig=go.Figure()
+        fig.add_trace(
+            go.Bar(x=target.index,y=target,
+                marker={'color':px.colors.qualitative.Dark24,
+                    'line':{'color':'black','width':3},
+                    'pattern':{'shape':'/'}})
+        )
+
+        fig.update_layout(go.Layout(title={'text':'Frequency of Orders by Hour of Day',
+                                    'font':{'color':'blue','size':30}},
+                                xaxis={'title':{'text':'Hour_of_Day'},
+                                    'gridwidth':1,'showgrid':True},
+                                yaxis={'title':{'text':'Frequency'},
+                                    'gridwidth':1,'showgrid':True},
+                                margin=dict(l=23,r=23,b=23)
+
+        ))
+        st.plotly_chart(fig,use_container_width=True)
+    
+    cols=st.multiselect("select n columns",df.department.unique())
+    # st.write('selecting columns:',cols)
+    st.write(cols)
       
 if __name__ == '__main__':
     main()
