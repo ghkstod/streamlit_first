9f0f333f001b9d9b8085ba21a0b24bd17baafe13
hanwool <na990312@naver.com>
2024-01-30 12:36:38 +0900

    1st commit: create streamlit page

---
+++ app.py
@@ -0,0 +1,15 @@
+# -*- codin:utf-8 -*-
+
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+def main():
+    st.title("START")
+    
+if __name__ == '__main__':
+    main()
+    
